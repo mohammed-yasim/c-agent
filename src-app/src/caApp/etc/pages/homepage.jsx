24ca96f04d5ed38508ba7bd516f8b5397e7e8b62
mohammed-yasim <mohammedyasim.edkm@gmail.com>
2023-11-11 00:51:35 +0530

    update_new

--- src-app/src/caApp/etc/pages/homepage.jsx
+++ src-app/src/caApp/etc/pages/homepage.jsx
@@ -32,9 +32,9 @@ function HomePage() {
 }
 function Hero() {
     return (
-        <div className="bg-white" style={{backgroundImage:"url(https://images.unsplash.com/photo-1506260408121-e353d10b87c7?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"}}>
-            <div className="mx-auto max-w-7xl sm:px-6 sm:py-32 lg:px-8 h-screen sm:h-auto">
-                <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        <div className="bg-white" style={{backgroundImage:"url(https://images.unsplash.com/photo-1506260408121-e353d10b87c7?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",backgroundSize:'cover',backgroundPosition:'center'}}>
+            <div className="mx-auto max-w-7xl px-6 py-32 lg:px-8 h-screen">
+                <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
                     <svg
                         viewBox="0 0 1024 1024"
                         className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
@@ -50,9 +50,9 @@ function Hero() {
                     </svg>
                     <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
                         <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                            Streamlining Water Management and
+                            Streamlining and Billing
                             <br />
-                            Billing for a Sustainable Future.
+                             for a Sustainable Future.
                         </h2>
                         <p className="mt-6 text-lg leading-8 text-gray-300">
                             Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
@@ -72,7 +72,7 @@ function Hero() {
                     </div>
                     <div className="relative mt-16 h-80 lg:mt-8">
                         <img
-                            className="absolute left-0 top-[2rem] w-[48rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+                            className="absolute left-0 top-0 bottom-0 w-[48rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
                             src="https://images.unsplash.com/photo-1520254553641-2eed4cf2ef26?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="App screenshot"
                         />
